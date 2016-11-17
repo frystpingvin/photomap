77302818f6ebd47e6bf541b9223accdfa4580be4
Samuel Lie <samuel.lie1@gmail.com>
2016-11-17 13:03:13 +0100

    Add thumbs to map, show images on marker click

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,7 +1,9 @@
 // This is where it all goes :)
+var map;
+
 function initMap() {
   // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('photomap'), {
+  map = new google.maps.Map(document.getElementById('photomap'), {
     center: {lat: 59.911491, lng: 10.757933},
     zoomControl: false,
     streetViewControl: false,
@@ -9,49 +11,94 @@ function initMap() {
     rotateControl: false,
     zoom: 7
   });
+  map.addListener('click', hideImage);
+  map.addListener('drag', hideImage);
 }
 
-(function () {
-  var igApi      = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=41682531.1677ed0.0345e6d7b61142afab3a948804bafcdc&count=33&callback=?',
-      igData,
-      igDataArr  = [];
+var apiCall   = 'https://api.flickr.com/services/rest/?method=flickr.photos.getWithGeoData&api_key=8ee1397e332c04f76981c9000befad6a&format=json&nojsoncallback=1&auth_token=72157676598877756-abf3fd6d5247e12d&api_sig=e91fc6039e9942533336658338bd7431',
+    photos    = [],
+    body      = $('body'),
+    photoEl   = $('#photoinfo'),
+    photoOpen = false;
 
-  var instagramData;
+var getPhotos = function(callback) {
+  $.when(
+    $.getJSON(apiCall, function(data) {
+      data = data;
+    })
+  ).then(function(data) {
+    var photosLength = data.photos.photo.length;
 
+    $.each(data.photos.photo, function(i, photoData) {
+      var photo = {};
 
+      photo.farm   = photoData.farm;
+      photo.id     = photoData.id;
+      photo.secret = photoData.secret;
+      photo.server = photoData.server;
 
-  var getIgPhotosLocation = function(api, data, array) {
-    $.when(
-      $.getJSON(api, function(jsonData) {
-        data = jsonData;
-      })
-    ).then(function () {
-      if (data) {
-        if (data.pagination) {
-          console.log(data.pagination.next_url);
+      $.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.geo.getLocation&api_key=8ee1397e332c04f76981c9000befad6a&photo_id=' + photo.id + '&format=json&nojsoncallback=1', function(data) {
+        console.log(data);
+        photo.latitude  = data.photo.location.latitude;
+        photo.longitude = data.photo.location.longitude;
+        photos.push(photo);
+
+        if (photos.length == photosLength) {
+          console.log('callback');
+          callback(photos);
         }
+      });
 
-        $.each(data.data, function(i, photoData) {
-          if (photoData.location) {
-            createPhotoObjectToArr(photoData, array);
-          }
-        });
-      } else {
-        console.log('there was an error collecting data');
-      }
     });
-  };
 
+  }).fail(function() {
+    console.log('error');
+  });
+};
 
+getPhotos(function(photos) {
+  if (photos) {
+    $.each(photos, function(i, photo) {
+      var latitude    = photo.latitude;
+      var longitude   = photo.longitude;
+      var thumbnail   = '//farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_t.jpg';
+      var image       = '//farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_h.jpg';
+      var latLng      = new google.maps.LatLng(latitude, longitude);
+      var marker      = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        icon: thumbnail
+      });
+      marker.addListener('click', function() {
+        showImage(image, marker.getPosition(), map.getBounds());
+      });
+    });
+  }
+});
 
-  var createPhotoObjectToArr = function(data, array) {
-    var photoObject = {};
-    photoObject.id = data.id;
-    photoObject.latitude = data.location.latitude;
-    photoObject.longitude = data.location.longitude;
-    photoObject.locationName = data.location.name;
-    array.push(photoObject);
-  };
+var showImage = function(photo, markerPosition, mapBounds) {
+  var mapBoundNorth = mapBounds.getNorthEast().lat();
+  var mapBoundSouth = mapBounds.getSouthWest().lat();
+  var markerLat     = markerPosition.lat();
+  var latitude      = markerLat - ((mapBoundNorth - mapBoundSouth) * 0.25);
+  var longitude     = markerPosition.lng();
+  map.panTo({lat: latitude, lng: longitude});
 
-  getIgPhotosLocation(igApi, igData, igDataArr);
-}());
+  if (photoOpen) {
+    photoEl.empty();
+  } else {
+    body.attr('id', 'do-show-photo');
+    photoOpen = true;
+  }
+
+  photoEl.append('<img src="' + photo + '">');
+};
+
+var hideImage = function() {
+  if (photoOpen) {
+    console.log('hide image');
+    body.removeAttr('id', 'do-show-photo');
+    photoEl.empty();
+  }
+  photoOpen = false;
+}
